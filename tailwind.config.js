4b684b023619857154c903311485a9cc00210870
msuito <matiassuito@gmail.com>
2025-11-11 23:43:13 -0300

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class", // 👈 esta línea activa el modo oscuro controlado por clase
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ["Inter", "system-ui", "sans-serif"],
       },
       colors: {
         green: {
-          700: '#3f5f3a',
-          800: '#2f4a2a',
-          900: '#1f351d',
+          700: "#3f5f3a",
+          800: "#2f4a2a",
+          900: "#1f351d",
         },
       },
     },
